33baf426487097f3fee7162d1d5e7b729b4cdb47
Milinbhakta <desire2dosomething@gmail.com>
2023-12-30 20:18:37 -0400

    Add hbs package and update app to render containers view

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,18 +4,27 @@ import cors from "cors";
 import api from "./api";
 import ErrorHandler from "./middlewares/ErrorHandler";
 import NotFound from "./middlewares/NotFound";
-import VerifyApiKey from "./middlewares/VerifyApiKey";
+import docker from "./utils/docker";
 
 require("dotenv").config();
 
 const app = express();
 
+app.set("views", "./src/views");
+app.set("view engine", "hbs");
+app.set("view options", { layout: "index" });
+
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-// Add the verifyApiKey middleware
-app.use(VerifyApiKey);
+app.get("/", async (req, res) => {
+  const containers = await docker.listContainers();
+  res.render("partials/containers", {
+    layout: "index",
+    containers,
+  });
+});
 
 app.use("/api/v1", api);
 
